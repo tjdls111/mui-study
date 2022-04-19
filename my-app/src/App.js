8d43c3a5b5cf506620b070f0ffb361b0139683f0
Seoin <tjdls111@naver.com>
2022-04-19 15:56:59 +0900

    필요 없는 파일 제거, 버튼

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
+import react, { useState } from "react";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Article from "./components/Article";
 
 function App() {
+  const [value, setValue] = useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-      <Button variant="contained">Hello World</Button>;
+      <Header />
+      <Nav />
+      <Article />
+      <Button variant="outlined">Create</Button>
     </div>
   );
 }
---
+++ my-app/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+      <ol>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+      </ol>
+    </>
+  );
+}
+
+export default Nav;
