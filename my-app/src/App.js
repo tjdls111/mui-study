899b04fc9d6e64f66116a45a0b637ea8c51d0261
Seoin <tjdls111@naver.com>
2022-04-19 16:04:08 +0900

    container, grid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,19 +6,34 @@ import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Article from "./components/Article";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { Container, Grid } from "@mui/material";
 
 function App() {
   const [value, setValue] = useState(30);
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   return (
     <div className="App">
-      <Header />
-      <Nav />
-      <Article />
-      <Button variant="outlined">Create</Button>
+      <Container fixed>
+        <Header />
+        <Grid container>
+          <Grid item xs={2}>
+            <Nav />
+          </Grid>
+          <Grid item xs={10}>
+            <Article />
+          </Grid>
+        </Grid>
+        <ButtonGroup>
+          <Button variant="outlined">Create</Button>
+          <Button variant="outlined">Update</Button>
+        </ButtonGroup>
+        <Button variant="outlined" color="warning">
+          Delete
+        </Button>
+      </Container>
     </div>
   );
 }
--- my-app/src/components/Article.jsx
+++ my-app/src/components/Article.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Article() {
   return (
-    <div>Article</div>
-  )
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+      similique repellat autem accusamus nam amet neque doloremque!
+      Necessitatibus explicabo, ex nihil nisi in a ad dignissimos ut impedit
+      laboriosam quam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Eaque non adipisci quia aperiam laboriosam tempore quo officiis fuga totam
+      quasi? Ad architecto molestiae magnam facilis minima porro, doloremque
+      itaque odit!
+    </div>
+  );
 }
 
-export default Article
+export default Article;
